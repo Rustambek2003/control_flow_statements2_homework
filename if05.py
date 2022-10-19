03cd83125ee480d194a940f966f875cbcf28c430
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-10-19 18:20:00 +0500

    if_05

--- if05.py
+++ if05.py
@@ -6,4 +6,29 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1 = n % 10
+    n //= 10
+
+    x2 = n % 10
+    n //= 10
+
+    x3 = n % 10
+    n //= 10
+
+    x4 = n % 10
+    n //= 10
+
+    x5 = n % 10
+
+    s = x1
+    if s < x2:
+        s = x2
+    if s < x3:
+        s = x3
+    if s < x4:
+        s = x4
+    if s < x5:
+        s = x5
+    
+    return s
+print(main(52698))
