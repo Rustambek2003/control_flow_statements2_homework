0321bdcda243f2fb42321583c42b0090da2a538c
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-10-20 11:22:56 +0500

    if_03

--- if03.py
+++ if03.py
@@ -8,11 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    if a >= b and b >= c:
+    if (a >= b and b >= c) or (c >= b and b >= a):
         s = b
-    if b >= a and a >= c:
+    if (b >= a and a >= c) or (c >= a and a >= b):
         s = a
-    if b >= c and c >= a:
+    if (b >= c and c >= a) or (a >= c and c >= b):
         s = c
     return s
-print(main(5,5,-1))
+print(main(5,5,5))
